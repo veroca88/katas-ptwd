6bd55b5090f612590f0beecdc65f95b8cf839108
Veronica Donoso <veronicadonoso1506@gmail.com>
2020-03-04 13:12:06 -0500

    katas js

---
+++ kata-concat-first-second-diagonal.js
@@ -0,0 +1,64 @@
+# Coding challenge: Concat first/second diagonal
+
+## Instructions
+
+### Iteration 1
+From a two dimensional array (with the same height and width), extract and concat all the elements from the "first diagonal", starting from the top left corner to the bottom right corner.
+
+```js
+function concatFirstDiagonal(twoDimArray) {
+    // your code goes here
+}
+```
+If given the following array, here is the result you should expect:
+
+```js
+const someArr1 = [
+  ['L','z','x','C','v','B'],
+  ['t','i','I','p','e','7'],
+  ['w','y','s','r','m','x'],
+  ['q','U','l','b','k','H'],
+  ['A','i','G','Y','o','L'],
+  ['n','f','d','W','z','n']
+];
+
+// solution:
+console.log(concatFirstDiagonal(someArr1));
+// ==> "Lisbon"
+```
+
+### Iteration 2
+
+From a two dimensional array (with the same height and width), extract and concat all the elements from the "second diagonal", starting from the top right corner to the bottom left corner.
+
+```js
+function concatSecondDiagonal(twoDimArray) {
+    // your code goes here
+}
+```
+If given the following array, here is the result you should expect:
+
+```js
+const someArr2 = [
+  ['L','z','x','C','v','B'],
+  ['t','i','I','p','e','7'],
+  ['w','y','s','r','m','x'],
+  ['q','U','l','b','k','H'],
+  ['A','i','G','Y','o','L'],
+  ['n','f','d','W','z','n']
+];
+
+// solution:
+console.log(concatSecondDiagonal(someArr2));
+// ==> "Berlin"
+```
+
+## Submission
+Submitting katas is not mandatory. We will provide solutions on or after every class or at the end of the week, depending on how we agree on it. 
+
+However, you should save your work on your GitHub profile. If you still haven't created a repo with katas on your GitHub profile, this should be an excellent moment to do so. 
+
+Please create a new file locally and push it on the remote repository with all the katas (on your GH profile). If you have any doubts on how to connect your local and remote repos, please refer to the following docs: [How to connect local and remote repos?](https://gist.github.com/sandrabosk/ce8955324a1e38c022dcf87eab6f03fc)
+
+
+Happy coding! :blue_heart: 
